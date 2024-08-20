8d1c11dd6e331e98ccfc44ca34bb9408bfae4a8d
Samriddhi Shukla <101646975+Samriddhi430@users.noreply.github.com>
2024-08-20 20:56:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Burning Tree/burning-tree.cpp
+++ Difficulty: Hard/Burning Tree/burning-tree.cpp
@@ -96,65 +96,82 @@ struct Node {
 */
 class Solution {
   public:
-  Node* makenodetoparentmapping(Node* root,unordered_map<Node*,Node*>& parentmap,int target){
-      queue<Node*> q;
-      Node* targetnode=0;
-      q.push(root);
-      parentmap[root]=0;
-      while(!q.empty()){
-          Node* front=q.front();
-          q.pop();
-          if(front->data==target){
-              targetnode=front;
-          }
-          if(front->left){
-              q.push(front->left);
-              parentmap[front->left]=front;
-          }
-          if(front->right){
-              q.push(front->right);
-              parentmap[front->right]=front;
-          }
-      }
-      return targetnode;
-  }
-  int burnthetree(Node* targetnode,unordered_map<Node*,Node*>& parentmap){
-      unordered_map<Node*,bool> isburnt;
-      queue<Node*> q;
-      int t=0;
-      q.push(targetnode);
-      isburnt[targetnode]=1;
-      while(!q.empty()){
-          int size=q.size();
-          bool isfirespreaded=0;
-          for(int i=0;i<size;++i){
-              Node* front=q.front();
-              q.pop();
-              if(front->left && !isburnt[front->left]){
-                  q.push(front->left);
-                  isburnt[front->left]=1;
-                  isfirespreaded=1;
-              }
-              if(front->right && !isburnt[front->right]){
-                  q.push(front->right);
-                  isburnt[front->right]=1;
-                  isfirespreaded=1;
-              }
-              if(parentmap[front] && !isburnt[parentmap[front]]){
-                  q.push(parentmap[front]);
-                  isburnt[parentmap[front]]=1;
-                  isfirespreaded=1;
-              }
-          }
-          if(isfirespreaded) ++t;
-      }  
-      return t;
-  }
+     int helper(Node* targetNode,unordered_map<Node*,Node*>&childToParent){
+        int maxi=0;
+        queue<Node*>q;
+        q.push(targetNode);
+        unordered_map<Node*,int>vis;
+        vis[targetNode]=1;
+        
+        while(!q.empty()){
+            int size=q.size();
+            bool flag=0;
+            for(int i=0;i<size;i++){
+                Node* old=q.front();
+                q.pop();
+                
+                if(old->left && !vis[old->left]){
+                    flag=1;
+                    vis[old->left]=1;
+                    q.push(old->left);
+                }
+                
+                if(old->right && !vis[old->right]){
+                    flag=1;
+                    vis[old->right]=1;
+                    q.push(old->right);
+                }
+                
+                if(childToParent[old] && !vis[childToParent[old]]){
+                    flag=1;
+                    vis[childToParent[old]]=1;
+                    q.push(childToParent[old]);
+                }
+            }
+            if(flag) maxi++;
+        }
+        
+        return maxi;
+    }
+    
+    Node* levelOT(Node* root,unordered_map<Node*,Node*>&childToParent,
+    int target){
+        Node* result=NULL;
+        queue<Node*>q;
+        Node* node=root;
+        q.push(node);
+        
+        while(!q.empty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                Node* old=q.front();
+                if(old->data==target) result=old;
+                q.pop();
+                
+                if(old->left){
+                    childToParent[old->left]=old;
+                    q.push(old->left);
+                }
+                
+                if(old->right){
+                    childToParent[old->right]=old;
+                    q.push(old->right);
+                }
+            }
+        }
+        
+        return result;
+    }
+  
     int minTime(Node* root, int target) 
     {
-        unordered_map<Node*,Node*> parentmap;
-        Node* targetnode= makenodetoparentmapping(root,parentmap,target);
-        return burnthetree(targetnode,parentmap);
+        // Your code goes here
+        unordered_map<Node*,Node*>childToParent;
+        Node* targetNode=levelOT(root,childToParent,target);
+        int minTime=helper(targetNode,childToParent);
+        
+        return minTime;
+    
         // Your code goes here
     }
 };
